dd00c80461642bec14b96ed907150df8a30b6b77
gosaenz <gosaenz@hotmail.com>
2017-09-18 22:20:30 -0600

    Text input

--- SemanticAnalysis/LexYacc/main.cpp
+++ SemanticAnalysis/LexYacc/main.cpp
@@ -1,5 +1,24 @@
 #include "y.tab.c"
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
 main(){
 	if(yyparse()==0)
 	printf("\nAnalisis Sintactico concluido sin errores\n");
+}
+
+int main (){
+    string fileIN;
+    string result;
+	ifstream infile;
+	infile.open ("test.txt");
+    while(!infile.eof()) {
+	    getline(infile,fileIN); 
+	    result = result + fileIN;
+	}
+    cout<< result;
+	infile.close();
+
+	return 0;
 }
