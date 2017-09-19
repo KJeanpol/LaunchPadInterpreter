4a18a9069f6b277d20c195e4d46eef67621575c2
gosaenz <gosaenz@hotmail.com>
2017-09-18 22:28:50 -0600

    Reset

--- SemanticAnalysis/LexYacc/main.cpp
+++ SemanticAnalysis/LexYacc/main.cpp
@@ -1,24 +1,8 @@
 #include "y.tab.c"
-#include <iostream>
-#include <fstream>
-#include <string>
-using namespace std;
+
 main(){
 	if(yyparse()==0)
 	printf("\nAnalisis Sintactico concluido sin errores\n");
 }
 
-int main (){
-    string fileIN;
-    string result;
-	ifstream infile;
-	infile.open ("test.txt");
-    while(!infile.eof()) {
-	    getline(infile,fileIN); 
-	    result = result + fileIN;
-	}
-    cout<< result;
-	infile.close();
 
-	return 0;
-}
