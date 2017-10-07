38f3c3655df6139847dc10e950e832bb854a15b8
JoseRIvera07 <32539452+JoseRIvera07@users.noreply.github.com>
2017-10-07 13:49:45 -0600

    Vector generico en LOOP para datos en BT1.readString()

--- prograBluetooth/prograBluetooth.ino
+++ prograBluetooth/prograBluetooth.ino
@@ -5,10 +5,10 @@
 SoftwareSerial BT1(12, 13); // RX, TX recorder que se cruzan
 
 Vector <String> vector;
-char cadena[255];
-int i = 0;
 int flag = 0;
-StaticJsonBuffer<1000> jsonBuffer;
+String value2;
+String value1;
+
 
 
 
@@ -25,117 +25,110 @@ void setup()
 void loop() {
   if (BT1.available()) {
     read();
-    Serial.print(vector[0]);
-    Serial.print(vector[1]);
-    Serial.print(vector[2]);
-    Serial.print(vector[3]);
-    Serial.print(vector[4]);
-   
+    Serial.print(vector[9]);
   }
+
 }
 
 
-  void clean()
-  {
-    for (int cl = 0; cl <= i; cl++)
-    {
-      cadena[cl] = 0;
-    }
-    i = 0;
-  }
+//...............READSOCKETANDSAVE............//
+void read() {
 
-  //...............READSOCKETANDSAVE............//
-  void read() {
+  String json = BT1.readString();
+  splitter(json);
 
-    String json = BT1.readString();
-    //Serial.print(json);
-    splitter(json);
-    
 }
 
+
+
+
+//...............TOKENCOUNT............//
+//ESTA FUNCION SE CREO POR QUE EL LOOP DE CREACION DE LOS VECTORES HACIA UQE AL INSTANCIARLOS AUN NO SE HABIAN CREADO LO CUAL PROVOCABA
+//UN ERROR EN TIEMPO DE EJECUCION YA QUE SE DEBIA DE ESPERAR UN MOMENTO PARA QUE SE CREARAN Y LUEGO INSTANCIARLOS PERO NO HABIA FORMA YA QUE EL DILAY() PAUSA EL EVENTO PERO ESO
+//INVOLUCRABA DETENER TODO ANTES DE LA LINA DE CODIGO DONDE SE DEVUELVE PARA CREARSE, POR LO CUAL SE OPTA POR OBTENER TODO EL STRING PARA LUEGO SPLITEARLO Y ACOMODAR TODO EN TIEMPO DE COMPILACION
+void splitter(String Q) {
+  for (int i = 0; i < Q.length(); i++) {
+    if (Q.substring(i, i + 1) =="}") {
+        value1 = Q.substring(0, i + 1);
+        value2 = Q.substring(i + 1);
+        vector.push_back(value1);
+       // Serial.print("ESTE ES EL NUEVO VALOR......................");
+        //Serial.print(value1);
+        //Serial.println("......>>>>>>...............................>>>>................");
+        splitter(value2);break;}
+        
+        
+//        
+//        if (value2 == '\0') {
+//          Serial.println("si entra");
+//          break;
+//        
+//          
+//        }
+        
+        
+
+
+      }
+      
+      
+
     
     
-//    char dato = BT1.read();
-//    cadena[i++] = dato;
-//    Serial.print(dato);
-//    if (dato == '}') {
-//      vector.push_back(cadena);
-//      clean();
-//      //Serial.print("hola");
-//  }
 
-  
+}
 
-  //...............TOKENCOUNT............//
-  //ESTA FUNCION SE CREO POR QUE EL LOOP DE CREACION DE LOS VECTORES HACIA UQE AL INSTANCIARLOS AUN NO SE HABIAN CREADO LO CUAL PROVOCABA
-  //UN ERROR EN TIEMPO DE EJECUCION YA QUE SE DEBIA DE ESPERAR UN MOMENTO PARA QUE SE CREARAN Y LUEGO INSTANCIARLOS PERO NO HABIA FORMA YA QUE EL DILAY() PAUSA EL EVENTO PERO ESO
-  //INVOLUCRABA DETENER TODO ANTES DE LA LINEA DE CODIGO DONDE SE DEVUELVE PARA CREARSE, POR LO CUAL SE OPTA POR OBTENER TODO EL STRING PARA LUEGO SPLITEARLO Y ACOMODAR TODO EN TIEMPO DE COMPILACION
-  void splitter(String Q){               
-    for (int i = 0; i < Q.length(); i++) {
-  if (Q.substring(i, i+1) == "}") {
-    String value1 = Q.substring(0, i+1);
-    vector.push_back(value1);
-    String value2= Q.substring(i+1);
-    
-    //Serial.print(value1);
-    splitter(value2);
-    
-    
-    break;
-  }
-    
-  }
+
+
+
+
+
+
+//................PARSERJUEGO...............//
+
+int parserJuego(String Json) {
+  StaticJsonBuffer<200> jsonBuffer;
+  JsonObject& root = jsonBuffer.parseObject(Json);
+  // Test if parsing succeeds.
+  if (!root.success()) {
+    Serial.println("parseObject() failed");
+    return;
   }
 
+  int juego = root["Juego"];
+  return juego;
 
+}
 
+//................PARSERBOTON..............//
 
-
-  //................PARSERJUEGO...............//
-
-  int parserJuego(String Json) {
-    StaticJsonBuffer<200> jsonBuffer;
-    JsonObject& root = jsonBuffer.parseObject(Json);
-    // Test if parsing succeeds.
-    if (!root.success()) {
-      Serial.println("parseObject() failed");
-      return;
-    }
-
-    int juego = root["Juego"];
-    return juego;
-
+int parserBoton(String Json) {
+  StaticJsonBuffer<200> jsonBuffer;
+  JsonObject& root = jsonBuffer.parseObject(Json);
+  // Test if parsing succeeds.
+  if (!root.success()) {
+    Serial.println("parseObject() failed");
+    return;
   }
 
-  //................PARSERBOTON..............//
+  int boton = root["Boton"];
+  return boton;
 
-  int parserBoton(String Json) {
-    StaticJsonBuffer<200> jsonBuffer;
-    JsonObject& root = jsonBuffer.parseObject(Json);
-    // Test if parsing succeeds.
-    if (!root.success()) {
-      Serial.println("parseObject() failed");
-      return;
-    }
-
-    int boton = root["Boton"];
-    return boton;
-
+}
+//................PARSERFUNCTION..............//
+String parserFunction(String Json) {
+  StaticJsonBuffer<200> jsonBuffer;
+  JsonObject& root = jsonBuffer.parseObject(Json);
+  // Test if parsing succeeds.
+  if (!root.success()) {
+    Serial.println("parseObject() failed");
+    return;
   }
-  //................PARSERFUNCTION..............//
-  String parserFunction(String Json) {
-    StaticJsonBuffer<200> jsonBuffer;
-    JsonObject& root = jsonBuffer.parseObject(Json);
-    // Test if parsing succeeds.
-    if (!root.success()) {
-      Serial.println("parseObject() failed");
-      return;
-    }
 
-    String type = root["Type"];
-    return type;
+  String type = root["Type"];
+  return type;
 
-  }
+}
 
 
 
